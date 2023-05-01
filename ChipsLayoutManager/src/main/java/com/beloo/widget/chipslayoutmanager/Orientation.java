1dc65dccbce5c94b85d99aa1eba57b4d1b549f12
uladzislau <anrimian@gmail.com>
2023-05-01 12:23:35 +0200

    * try to fix java version

--- ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/Orientation.java
+++ ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/Orientation.java
@@ -2,6 +2,10 @@ package com.beloo.widget.chipslayoutmanager;
 
 import androidx.annotation.IntDef;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 @IntDef({ChipsLayoutManager.HORIZONTAL, ChipsLayoutManager.VERTICAL})
+@Retention(RetentionPolicy.SOURCE)
 @interface Orientation {
 }
